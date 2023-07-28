9dae398acf27119e7c5d047c06c127ad8c5f14f9
fto-k <fto@kovalee.app>
2023-07-28 15:04:27 +0200

    feat: created Swift Package for handling KovaleeSDK attribution

--- Package.swift
+++ Package.swift
@@ -1,28 +1,33 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "KovaleeAttribution",
+	defaultLocalization: "en",
+	platforms: [
+		.iOS(.v14)
+	],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "KovaleeAttribution",
-            targets: ["KovaleeAttribution"]),
+		.library(
+			name: "KovaleeAttribution",
+			targets: [
+				"KovaleeAttribution",
+			]
+		)
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+		.package(url: "https://github.com/cotyapps/Kovalee-iOS-SDK", branch: "sdk-spm-split"),
+		.package(url: "https://github.com/adjust/ios_sdk", from: Version(4, 33, 5))
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "KovaleeAttribution",
-            dependencies: []),
-        .testTarget(
-            name: "KovaleeAttributionTests",
-            dependencies: ["KovaleeAttribution"]),
+		.target(
+			name: "KovaleeAttribution",
+			dependencies: [
+				.product(name: "KovaleeSDK", package: "Kovalee-iOS-SDK"),
+				.product(name: "Adjust", package: "ios_sdk")
+			]
+		)
     ]
 )
